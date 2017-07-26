a947b905cf02bba4077ce6fecab40eef732b4a63
Ilya Monakov <imonakov@hostcomm.ru>
2017-07-26 20:15:57 +0300

    nuxt

---
+++ client/pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <table>
+    <tr v-for="game in games">
+      <td>{{ game.id }}</td>
+    </tr>
+  </table>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    asyncData() {
+        return axios.get('http://localhost:8000/instances/1/games/').then(
+            res => {return {
+                games: res.data.results
+            }}
+        )
+    },
+    data() {
+        return {
+            games: [],
+        }
+    }
+}
+</script>
+
+<style>
+</style>
