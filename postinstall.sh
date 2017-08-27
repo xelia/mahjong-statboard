f7bb02074cc7e75011288e81e57db58594c0c67e
Ilya <imonakov@gmail.com>
2017-08-27 13:13:11 +0300

    postinstall

--- postinstall.sh
+++ postinstall.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+
 cd server
+PIPENV_VENV_IN_PROJECT=1
 pipenv install
 DJANGO_SETTINGS_MODULE=mahjong_statboard.settings_prod
-python manage.py migrate
+pipenv run python manage.py migrate
 
 cd ../client
 npm update
