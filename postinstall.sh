ccc7d883428627e49bbf0b6d225e7c4b6cbd3efc
Ilya <imonakov@gmail.com>
2017-08-27 14:13:31 +0300

    fix

--- postinstall.sh
+++ postinstall.sh
@@ -3,7 +3,7 @@
 cd server
 PIPENV_VENV_IN_PROJECT=1
 pipenv install
-DJANGO_SETTINGS_MODULE=mahjong_statboard.settings_prod
+export DJANGO_SETTINGS_MODULE=mahjong_statboard.settings_prod
 pipenv run python manage.py migrate
 
 cd ../client
