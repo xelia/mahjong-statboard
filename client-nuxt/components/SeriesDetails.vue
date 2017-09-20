ddd6f42448cc023114c02c454cffbb8ddf6c3dd3
Ilya Monakov <imonakov@hostcomm.ru>
2017-09-20 18:47:37 +0300

    client-nuxt removed

--- client-nuxt/components/SeriesDetails.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <span><span v-for="place in series.series">{{ place }}</span></span>
-</template>
-<script>
-  export default {
-    props: ['series']
-  }
-</script>
--- client-nuxt/middleware/check-auth.js
+++
@@ -1,9 +0,0 @@
-import { getUserFromCookieServerSide, getUserFromCookieClientSide,  } from '~/utils/auth'
-
-export default function ({ app, isServer, store, req }) {
-   // If nuxt generate, pass this middleware
-  if (isServer && !req) return
-  const [token, loggedUser] = isServer ? getUserFromCookieServerSide(req) : getUserFromCookieClientSide()
-  store.commit('SET_USER', loggedUser)
-  store.commit('SET_TOKEN', token)
-}
