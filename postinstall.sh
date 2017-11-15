33f7704e4e63cc046d0fe7560a8de813f8d0bd5e
Ilya Monakov <imonakov@hostcomm.ru>
2017-11-15 17:16:04 +0300

    fix postinstall

--- postinstall.sh
+++ postinstall.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-cd server
-export PIPENV_VENV_IN_PROJECT=1
-pipenv install
-export DJANGO_SETTINGS_MODULE=mahjong_statboard.settings_prod
-pipenv run python manage.py migrate
-pipenv run python manage.py collectstatic --noinput
+cd server &&
+export PIPENV_VENV_IN_PROJECT=1 &&
+pipenv install &&
+export DJANGO_SETTINGS_MODULE=mahjong_statboard.settings_prod &&
+pipenv run python manage.py migrate &&
+pipenv run python manage.py collectstatic --noinput &&
 
-cd ../client
-npm update
-npm run build
-cd ..
+cd ../client &&
+npm update &&
+npm run build &&
+cd .. &&
 
 sudo supervisorctl restart mahjong_statboard
